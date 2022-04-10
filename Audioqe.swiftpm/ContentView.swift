da8b88b0e58efcc75e46e445f5c622804a9de3be
Jakub Florek <63607600+MAJKFL@users.noreply.github.com>
2022-04-11 00:32:36 +0200

    Added search bar

--- Audioqe.swiftpm/ContentView.swift
+++ Audioqe.swiftpm/ContentView.swift
@@ -4,13 +4,24 @@ import AVFoundation
 struct ContentView: View {
     @ObservedObject var queueList = QueueList()
     
+    @State private var searchText = ""
+    
+    var queues: [QueueEditor] {
+        if searchText == "" {
+            return queueList.queues
+        } else {
+            return queueList.queues.filter({ $0.name.lowercased().contains(searchText.lowercased()) })
+        }
+    }
+    
     var body: some View {
         NavigationView {
             List {
-                ForEach(queueList.queues) { queue in
+                ForEach(queues) { queue in
                     NavigationLink(destination: MainEditorView(editor: queue), label: { Label(queue.name, systemImage: "arrow.right.square") })
                 }
                 .onDelete(perform: queueList.remove)
+                .searchable(text: $searchText)
             }
             .listStyle(.sidebar)
             .navigationTitle("Queues")
